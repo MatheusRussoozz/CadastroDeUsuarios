ff68fa90cb567412e4ad8b6558371e7f944c105a
Matheus Henrique Barros Russo dos Santos <matheusrusso@MacBook-Air-de-Matheus.local>
2026-04-09 19:58:50 -0300

    feat: create Usuario entity and tb_usuarios table

---
+++ src/main/java/com/matheus/CadastroDeUsuarios/Usuario/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.matheus.CadastroDeUsuarios.Usuario.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @ManyToOne
+    @JoinColumn(name = "tb_tarefas")
+    private Tarefas tarefas;
+
+}
