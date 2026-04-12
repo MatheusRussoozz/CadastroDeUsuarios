e1f2d23369613e8e55f31b4ddd75c2a5da459488
Matheus Henrique Barros Russo dos Santos <matheusrusso@MacBook-Air-de-Matheus.local>
2026-04-12 13:43:12 -0300

    refactor: rename class name for UsuarioMapperInfos

---
+++ src/main/java/com/matheus/CadastroDeUsuarios/Usuario/mapper/UsuarioMapperInfos.java
@@ -0,0 +1,29 @@
+package com.matheus.CadastroDeUsuarios.Usuario.mapper;
+
+import com.matheus.CadastroDeUsuarios.Usuario.dtos.UsuarioDtoListarInformacoes;
+import com.matheus.CadastroDeUsuarios.Usuario.model.Usuario;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioMapperInfos {
+
+    public Usuario map(UsuarioDtoListarInformacoes usuarioDtoListarInformacoes){
+        Usuario usuario = new Usuario();
+        usuario.setId(usuarioDtoListarInformacoes.getId());
+        usuario.setEmail(usuarioDtoListarInformacoes.getEmail());
+        usuario.setNome(usuarioDtoListarInformacoes.getNome());
+        usuario.setTarefas(usuarioDtoListarInformacoes.getTarefas());
+
+        return usuario;
+    }
+
+    public UsuarioDtoListarInformacoes map(Usuario usuario){
+        UsuarioDtoListarInformacoes usuarioDtoListarInformacoes = new UsuarioDtoListarInformacoes();
+        usuarioDtoListarInformacoes.setId(usuario.getId());
+        usuarioDtoListarInformacoes.setEmail(usuario.getEmail());
+        usuarioDtoListarInformacoes.setNome(usuario.getNome());
+        usuarioDtoListarInformacoes.setTarefas(usuario.getTarefas());
+
+        return usuarioDtoListarInformacoes;
+    }
+}
