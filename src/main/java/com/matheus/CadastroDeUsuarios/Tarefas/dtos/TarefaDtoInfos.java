3b9006ffee87f477cbd0ffebde8c6b92f672d694
Matheus Henrique Barros Russo dos Santos <matheusrusso@MacBook-Air-de-Matheus.local>
2026-04-13 15:00:43 -0300

    feat: create TarefaDtoInfos

---
+++ src/main/java/com/matheus/CadastroDeUsuarios/Tarefas/dtos/TarefaDtoInfos.java
@@ -0,0 +1,23 @@
+package com.matheus.CadastroDeUsuarios.Tarefas.dtos;
+
+import com.matheus.CadastroDeUsuarios.Tarefas.model.Status;
+import com.matheus.CadastroDeUsuarios.Usuario.model.Usuario;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TarefaDtoInfos {
+
+    private String nome;
+    private String descricao;
+    private Status status;
+
+
+}
