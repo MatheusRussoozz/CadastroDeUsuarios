2bdc163c7027bb487e9c3c0b89f26ac6c822e630
Matheus Henrique Barros Russo dos Santos <matheusrusso@MacBook-Air-de-Matheus.local>
2026-04-14 14:55:52 -0300

    fix: correct toDto and toEntity methods in TarefasMapper

--- src/main/java/com/matheus/CadastroDeUsuarios/Tarefas/mapper/TarefasMapper.java
+++ src/main/java/com/matheus/CadastroDeUsuarios/Tarefas/mapper/TarefasMapper.java
@@ -7,17 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TarefasMapper {
 
-    public Tarefas toDto(TarefaDtoInfos tarefasDto){
-
-        Tarefas tarefas = new Tarefas();
-        tarefas.setNome(tarefasDto.getNome());
-        tarefas.setDescricao(tarefasDto.getDescricao());
-        tarefas.setStatus(tarefasDto.getStatus());
-
-        return tarefas;
-    }
-
-    public TarefaDtoInfos toEntity (Tarefas tarefas){
+    public TarefaDtoInfos toDto(Tarefas tarefas){
         TarefaDtoInfos tarefaDto = new TarefaDtoInfos();
 
         tarefaDto.setNome(tarefas.getNome());
@@ -26,5 +16,17 @@ public class TarefasMapper {
 
         return tarefaDto;
     }
+
+    public Tarefas toEntity(TarefaDtoInfos tarefasDto){
+        Tarefas tarefas = new Tarefas();
+
+        tarefas.setNome(tarefasDto.getNome());
+        tarefas.setDescricao(tarefasDto.getDescricao());
+        tarefas.setStatus(tarefasDto.getStatus());
+
+        return tarefas;
+    }
+
+
 
 }
